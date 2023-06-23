e1897a94fb5f367dcfb98953845e787e76dc2435
Jasmine Nabila Novel <jasminenabila19@gmail.com>
2023-06-23 13:40:09 +0700

    fixing code

--- Test Suites/TS001 - TSUser.ts
+++ Test Suites/TS001 - TSUser.ts
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteEntity>
    <description></description>
-   <name>TS001 - LoginUser</name>
+   <name>TS001 - TSUser</name>
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
